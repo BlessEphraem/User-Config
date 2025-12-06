83fd763af87aa3d81cea80c0efa5078744e392b6
Ephraem <113407330+BlessEphraem@users.noreply.github.com>
2025-12-06 11:47:36 +0100

    New Yasb Theme, and Komorebi configuration remade from scratch (plz kil me)

--- powershell/powershell_profile.ps1
+++ powershell/powershell_profile.ps1
@@ -1,4 +1,9 @@
-fastfetch
+# Open Windows Terminal
+# Click on the arrow -> Settings
+# In the left side panel, click on "Add a new profile"
+# Click on "New Empty Profile".
+# Inside the empty command line input box, put :
+# powershell.exe -NoExit -Command ". 'C:\Chemin\Vers\Votre\CustomProfile.ps1'"
 
 if ($env:PATHEXT -notlike "*.PY*") {
     $env:PATHEXT += ";.PY"
